1c1f28d9061bb5c0d43d03711a08761885791ecc
jackie-shawley <jackiekopco@aol.com>
2021-05-27 16:18:13 -0400

    order button

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ $(function () {
 
     $("#ingredientsModal").modal("show");
 
-    if ($.cookie("pop") === null) {
-        $("#mailListModal").modal("show");
-        $.cookie("pop", "1");
-    }
+    if (document.cookie.indexOf('modal_shown=') >= 0) {
+        //do nothing if modal_shown cookie is present
+       } else {
+         $('#mailListModal').modal('show');  //show modal pop up
+         document.cookie = 'modal_shown=seen'; //set cookie modal_shown
+         //cookie will expire when browser is closed
+       }
+       
 
 
 
